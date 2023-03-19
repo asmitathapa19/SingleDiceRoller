91ca480508c091ea60431e5b9f1e919baa950fe3
asmitathapa19 <116329561+asmitathapa19@users.noreply.github.com>
2023-03-19 07:47:37 +0545

    Making the dice roller functional

--- app/src/main/java/com/example/singlediceroller/MainActivity.java
+++ app/src/main/java/com/example/singlediceroller/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.singlediceroller;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button roll;
+    TextView result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        roll = findViewById(R.id.button);
+        result = findViewById(R.id.textView);
+
+        roll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random random = new Random();
+                int resultView = random.nextInt(6)+ 1;
+
+                result.setText(String.valueOf(resultView));
+            }
+        });
     }
 }
